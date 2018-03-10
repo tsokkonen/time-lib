ef115ecc7ef9793428c0d6ec5796a2fc4aef4ff6
Teemu Okkonen <tsokkonen@gmail.com>
2018-03-10 18:17:17 +0200

    init

---
+++ src/hello_time.clj
@@ -0,0 +1,17 @@
+(ns hello-time
+  (:require [clj-time.core :as t]
+            [clj-time.format :as f]))
+
+(defn now
+  "Returns the current datetime"
+  []
+  (t/now))
+
+(defn time-str
+  "Returns a string representation of a datetime in the local time zone."
+  [dt]
+  (f/unparse
+             (f/with-zone (f/formatter "hh:mm aa") (t/default-time-zone))
+             dt))
+
+            
